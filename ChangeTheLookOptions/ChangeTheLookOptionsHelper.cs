c93691fb669c812d7460b782bb959d7e3f0de2bb
Anoop Tatti <anoop.tatti@gmail.com>
2019-02-04 12:10:37 +0000

    Initial check in

---
+++ ChangeTheLookOptions/ChangeTheLookOptionsHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ChangeTheLookOptions
+{
+    public class ChangeTheLookOptionsHelper
+    {
+        internal static void ChangeHeaderLayout(string siteUrl, HeaderLayout layout, string userName, string password)
+        {
+            using (var client = new SPHttpClient(new Uri(siteUrl), userName, password))
+            {
+                var itemPayload = new { __metadata = new { type = "SP.Web" }, HeaderLayout = layout };
+                var endpointUrl = string.Format("{0}/_api/web", siteUrl);
+                var data = client.ExecuteJson(endpointUrl, HttpMethod.Post, itemPayload);
+            }
+        }
+
+        internal static void ChangeHeaderBackground(string siteUrl, HeaderBackground background, string userName, string password)
+        {
+            using (var client = new SPHttpClient(new Uri(siteUrl), userName, password))
+            {
+                var itemPayload = new { __metadata = new { type = "SP.Web" }, HeaderEmphasis = background };
+                var endpointUrl = string.Format("{0}/_api/web", siteUrl);
+                var data = client.ExecuteJson(endpointUrl, HttpMethod.Post, itemPayload);
+            }
+        }
+
+        internal static void ChangeMenuStyle(string siteUrl, MenuStyle menuStyle, string userName, string password)
+        {
+            bool megaMenuEnabled = menuStyle == MenuStyle.Megamenu ? true : false;
+            using (var client = new SPHttpClient(new Uri(siteUrl), userName, password))
+            {
+                var itemPayload = new { __metadata = new { type = "SP.Web" }, MegaMenuEnabled = megaMenuEnabled };
+                var endpointUrl = string.Format("{0}/_api/web", siteUrl);
+                var data = client.ExecuteJson(endpointUrl, HttpMethod.Post, itemPayload);
+            }
+        }
+
+    }
+
+    public enum HeaderLayout
+    {
+        Standard = 1,
+        Compact = 2
+    }
+
+    //These names are just for reference and are not the actual names
+    public enum HeaderBackground
+    {
+        Lighter,
+        Light,
+        Dark,
+        Darker
+    }
+
+    public enum MenuStyle
+    {
+        Megamenu,
+        Cascading
+    }
+}
--- ChangeTheLookOptions/Program.cs
+++ ChangeTheLookOptions/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChangeTheLookOptions
 {
@@ -10,6 +6,30 @@ namespace ChangeTheLookOptions
     {
         static void Main(string[] args)
         {
+            string userName = "username@tenant.onmicrosoft.com";
+            string password = "password";
+            string siteUrl = "https://tenant.sharepoint.com/sites/yoursite";
+            HeaderLayout layout = HeaderLayout.Standard;
+            HeaderBackground background = HeaderBackground.Darker;
+            MenuStyle menuStyle = MenuStyle.Cascading;
+
+            try
+            {
+                ChangeTheLookOptionsHelper.ChangeHeaderLayout(siteUrl, layout, userName, password);
+                Console.WriteLine($"Header Layout changed to {layout.ToString()} for {siteUrl}.");
+
+                ChangeTheLookOptionsHelper.ChangeHeaderBackground(siteUrl, background, userName, password);
+                Console.WriteLine($"Header Background changed to {background.ToString()} for {siteUrl}.");
+
+                ChangeTheLookOptionsHelper.ChangeMenuStyle(siteUrl, menuStyle, userName, password);
+                Console.WriteLine($"Menu Style changed to {menuStyle.ToString()} for {siteUrl}.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error " + ex.Message);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
         }
     }
 }
